1d484d4710a552d40ca0a1b6fa9ef11d90d26615
Ricardo Villa <ricardodanielvilla1@gmail.com>
2025-10-31 19:49:25 -0500

    Actualizando la WebApp Yapefy

--- public/assets/Tables-226c0b17.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-a8dfe74b.js";import{B as s}from"./Breadcrumb-e138aa63.js";const m=()=>e.jsxs(e.Fragment,{children:[e.jsx(s,{pageName:"Tables"}),e.jsx("div",{className:"flex flex-col gap-10"})]});export{m as default};
